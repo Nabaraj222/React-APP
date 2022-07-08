3f700a1598a6c892b1105770f9e1e0056f25205e
Nabaraj Ghimire <nabarajghimire222@gmail.com>
2022-07-08 21:03:11 +0545

    routes added

--- React-ClientApp/src/App.js
+++ React-ClientApp/src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import "bootstrap/dist/css/bootstrap.min.css";
 import React, { component } from "react";
+import AddPerson from "./components/add-person.component";
+import Person from "./components/person.component";
+import PersonList from "./components/person-list.component";
+import {Route, Routes, BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element = {<div> Home Page</div>} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
