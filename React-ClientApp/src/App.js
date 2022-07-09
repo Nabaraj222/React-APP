820311ee951d5a197cb2359927200fdbea35ec88
Nabaraj Ghimire <nabarajghimire222@gmail.com>
2022-07-10 00:14:56 +0545

    router pages implemented

--- React-ClientApp/src/App.js
+++ React-ClientApp/src/App.js
@@ -4,17 +4,46 @@ import React, { component } from "react";
 import AddPerson from "./components/add-person.component";
 import Person from "./components/person.component";
 import PersonList from "./components/person-list.component";
-import {Route, Routes, BrowserRouter } from 'react-router-dom';
+import {Link, Route, Routes, BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <div className='container'>
+          <Link to="/persons" className="navbar-brand">
+            PIS by Nabaraj
+          </Link>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to="/persons" className="nav-link">
+                Persons
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/add" className="nav-link">
+                Add
+              </Link>
+            </li>
+          </div>
+        </div>
+      </nav>
+      <div className="footer">
+      <footer className="py-5 bg-dark fixed-bottom">
+        <div className="container">
+          <p className="m-0 text-center text-white">
+            Copyright &copy; Nabaraj Ghimire 2022
+          </p>
+        </div>
+      </footer>
+      </div>
       <Routes>
-        <Route path='/' element = {<div> Home Page</div>} />
+        <Route exact path='/' element = {<PersonList/>} />
+        <Route path="/persons" element={<PersonList/>} />
+        <Route path="/add" element={<AddPerson/>} />
+        <Route path="/persons/:id" element={<Person/>} />
       </Routes>
-      </BrowserRouter>
     </div>
   );
 }
---
+++ React-ClientApp/src/services/person.service.js
@@ -0,0 +1,23 @@
+import http from "../http-common";
+
+class PersonDataService {
+  getAll() {
+    return http.get("/persons");
+  }
+  get(id) {
+    return http.get(`/persons/${id}`);
+  }
+  create(data) {
+    return http.post("/persons", data);
+  }
+  update(id, data) {
+    return http.put(`/persons/${id}`, data);
+  }
+  delete(id) {
+    return http.delete(`/persons/${id}`);
+  }
+  deleteAll() {
+    return http.delete(`/persons`);
+  }
+}
+export default new PersonDataService();
