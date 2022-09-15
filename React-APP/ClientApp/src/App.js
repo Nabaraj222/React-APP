61e6eeb25adf5b887a2b51d4062fc19a4c58cef0
Nabaraj Ghimire <nabarajghimire222@gmail.com>
2022-09-15 21:37:11 +0545

    Task#1 the css design have been improved.

--- React-APP/ClientApp/src/App.js
+++ React-APP/ClientApp/src/App.js
@@ -4,48 +4,48 @@ import React, { component } from "react";
 import AddPerson from "./components/add-person.component";
 import Person from "./components/person.component";
 import PersonList from "./components/person-list.component";
-import {Link, Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Link, Route, Routes, BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <div className='container'>
-          <Link to="/persons" className="navbar-brand">
-            PIS by Nabaraj
-          </Link>
-          <div className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link to="/persons" className="nav-link">
-                Persons
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/add" className="nav-link">
-                Add
-              </Link>
-            </li>
-          </div>
+    return (
+        <div className="App">
+            <nav className="navbar navbar-expand navbar-dark bg-dark">
+                <div className='container'>
+                    <Link to="/persons" className="navbar-brand">
+                        PIS by Nabaraj
+                    </Link>
+                    <div className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <Link to="/persons" className="nav-link">
+                                Persons
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/add" className="nav-link">
+                                Add
+                            </Link>
+                        </li>
+                    </div>
+                </div>
+            </nav>
+            <div className="footer">
+                <footer className="py-5 bg-dark fixed-bottom">
+                    <div className="container">
+                        <p className="m-0 text-center text-white">
+                            Copyright &copy; Nabaraj Ghimire {new Date().getFullYear()}
+                        </p>
+                    </div>
+                </footer>
+            </div>
+            <Routes>
+                <Route exact path='/' element={<PersonList />} />
+                <Route path="/persons" element={<PersonList />} />
+                <Route path="/add" element={<AddPerson />} />
+                <Route path="/persons/:id" element={<Person />} />
+            </Routes>
         </div>
-      </nav>
-      <div className="footer">
-      <footer className="py-5 bg-dark fixed-bottom">
-        <div className="container">
-          <p className="m-0 text-center text-white">
-            Copyright &copy; Nabaraj Ghimire 2022
-          </p>
-        </div>
-      </footer>
-      </div>
-      <Routes>
-        <Route exact path='/' element = {<PersonList/>} />
-        <Route path="/persons" element={<PersonList/>} />
-        <Route path="/add" element={<AddPerson/>} />
-        <Route path="/persons/:id" element={<Person/>} />
-      </Routes>
-    </div>
-  );
+    );
 }
 
 export default App;
--- React-APP/ClientApp/src/components/add-person.component.js
+++ React-APP/ClientApp/src/components/add-person.component.js
@@ -2,122 +2,136 @@ import React, { Component } from "react";
 import PersonDataService from "../services/person.service";
 
 class AddPerson extends Component {
-  constructor(props) {
-    super(props);
-    this.onChangeUserName = this.onChangeUserName.bind(this);
-    this.onChangeUserPassword = this.onChangeUserPassword.bind(this);
-    this.onChangeUserEmail = this.onChangeUserEmail.bind(this);
-    this.saveInformation = this.saveInformation.bind(this);
-    this.newInformation = this.newInformation.bind(this);
-    this.state = {
-      id: null,
-      userName: "",
-      userPassword: "", 
-      userEmail: ""
-    };
-  }
-  onChangeUserName(e) {
-    this.setState({
-      userName: e.target.value
-    });
-  }
-  onChangeUserPassword(e) {
-    this.setState({
-      userPassword: e.target.value
-    });
-  }
-  onChangeUserEmail(e) {
-    this.setState({
-      userEmail: e.target.value
-    });
-  }
-  saveInformation() {
-    var data = {
-      username: this.state.userName,
-      userPassword: this.state.userPassword,
-      userEmail: this.state.userEmail
-    };
-    PersonDataService.create(data)
-      .then(response => {
+    constructor(props) {
+        super(props);
+        this.onChangeUserName = this.onChangeUserName.bind(this);
+        this.onChangeUserPassword = this.onChangeUserPassword.bind(this);
+        this.onChangeUserEmail = this.onChangeUserEmail.bind(this);
+        this.saveInformation = this.saveInformation.bind(this);
+        this.newInformation = this.newInformation.bind(this);
+        this.state = {
+            id: null,
+            userName: "",
+            userPassword: "",
+            userEmail: ""
+        };
+    }
+    onChangeUserName(e) {
         this.setState({
-          id: response.data.id,
-          userName: response.data.userName,
-          userPassword: response.data.userPassword,
-          userEmail: response.data.userEmail,
+            userName: e.target.value
         });
-        alert("saved successfully.");
-      })
-      .catch(e => {
-        debugger;
-        alert(e);
-      });
-  }
-  newInformation() {
-    this.setState({
-      id: null,
-      userName: "",
-      userPassword: "",
-      userEmail: ""
-    });
-  }
-  render() {
-    return (
-      <div className="submit-form">
-        {this.state.submitted ? (
-          <div>
-            <h4>You submitted successfully!</h4>
-            <button className="btn btn-success" onClick={this.newInformation}>
-              Add
-            </button>
-          </div>
-        ) : (
-          <div className = "container">
-            <div className = "col-md-6">
-              <div className="form-group">
-              <label htmlFor="userName">UserName</label>
-              <input
-                type="text"
-                className="form-control"
-                id="userName"
-                required
-                value={this.state.userName} 
-                onChange={this.onChangeUserName}
-                name="userName"
-              />
-              </div>
-              <div className="form-group">
-              <label htmlFor="userPassword">User Password</label>
-              <input
-                type="password"
-                className="form-control"
-                id="userPassword"
-                required
-                value={this.state.userPassword}
-                onChange={this.onChangeUserPassword}
-                name="userPassword"
-              />
-              </div>
-              <div className="form-group">
-              <label htmlFor="userEmail">User Email</label>
-              <input
-                type="text"
-                className="form-control"
-                id="userEmail"
-                required
-                value={this.state.userEmail}
-                onChange={this.onChangeUserEmail}
-                name="userEmail"
-              />
-              </div>
-            <button onClick={this.saveInformation} className="btn btn-success">
-              Submit
-            </button>
+    }
+    onChangeUserPassword(e) {
+        this.setState({
+            userPassword: e.target.value
+        });
+    }
+    onChangeUserEmail(e) {
+        this.setState({
+            userEmail: e.target.value
+        });
+    }
+    saveInformation() {
+        var data = {
+            username: this.state.userName,
+            userPassword: this.state.userPassword,
+            userEmail: this.state.userEmail
+        };
+        PersonDataService.create(data)
+            .then(response => {
+                this.setState({
+                    id: '',
+                    userName: '',
+                    userPassword: '',
+                    userEmail: '',
+                });
+                alert(response.data.userName + " saved successfully.");
+            })
+            .catch(e => {
+                alert(e);
+            });
+    }
+    newInformation() {
+        this.setState({
+            id: null,
+            userName: "",
+            userPassword: "",
+            userEmail: ""
+        });
+    }
+    render() {
+        return (
+            <div>
+                {this.state.submitted ? (
+                    <div>
+                        <h4>You submitted successfully!</h4>
+                        <button className="btn btn-success" onClick={this.newInformation}>
+                            Add
+                        </button>
+                    </div>
+                ) : (
+                    <div className="container">
+                        <br />
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Add new Person</h5>
+                                <form>
+                                    <div class="form-group">
+                                        <label for="userName">User Name</label>
+                                        <input
+                                            type="text"
+                                            class="form-control"
+                                            id="userName"
+                                            required
+                                            value={this.state.userName}
+                                            onChange={this.onChangeUserName}
+                                            name="userName"
+                                            placeholder="Enter name.."
+                                        />
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label htmlFor="userPassword">Password</label>
+                                        <input
+                                            type="password"
+                                            class="form-control"
+                                            id="userPassword"
+                                            required
+                                            value={this.state.userPassword}
+                                            onChange={this.onChangeUserPassword}
+                                            name="userPassword"
+                                            placeholder="Enter password.."
+                                        />
+                                        <small id="passwordHelp" class="form-text text-muted">The password must be at least 8 digits with the at least one Alpha Numeric and Special character.</small>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label htmlFor="userEmail">User Email</label>
+                                        <input
+                                            type="text"
+                                            class="form-control"
+                                            id="userEmail"
+                                            required
+                                            value={this.state.userEmail}
+                                            onChange={this.onChangeUserEmail}
+                                            name="userEmail"
+                                            placeholder="Enter email.."
+                                        />
+                                    </div>
+                                    <br />
+                                    <div class="form-group">
+                                        <button onClick={this.saveInformation} class="btn btn-success">
+                                            Submit
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                )}
             </div>
-          </div>
-        )}
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default AddPerson;
